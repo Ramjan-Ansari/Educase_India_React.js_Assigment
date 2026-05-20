b265d01b62683870232069bd61405800ace303c6
Ramjan Ansari <ramjanansari32426@gmail.com>
2026-05-20 15:11:25 +0530

    fix path issues for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
-import Landingpage from './pages/Landingpage.jsx'
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
-import Profile from './pages/Profile.jsx';
-
+import Landingpage from '../src/pages/Landingpage.jsx'
+import Login from '../src/pages/Login.jsx';
+import Signup from '../src/pages/Signup.jsx';
+import Profile from '../src//pages/Profile.jsx';
 
 function App() {
 
